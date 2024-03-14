a30e76965e74c0a44dadb5a412d7c187f138c2b2
WoliBlue <trabajojosearenasvenegas@gmail.com>
2024-03-14 14:00:32 +0100

    Clase persona

---
+++ Personas/src/modelos/Persona.java
@@ -0,0 +1,72 @@
+package modelos;
+
+	
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * @param dni
+     * @param nombre
+     * @param apellido1
+     * @param apellido2
+     * @param edad
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellido1;
+
+    /**
+     * 
+     */
+    private String apellido2;
+
+    /**
+     * 
+     */
+    private int edad = 0;
+
+    /**
+     * @param dni 
+     * @param nombre 
+     * @param apellido1 
+     * @param apellido2 
+     * @param edad
+     */
+    public void Persona(String dni, String nombre, String apellido1, String apellido2, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @param dni
+     */
+    public void setDni(String dni) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getDni() {
+        // TODO implement here
+        return "";
+    }
+
+}
